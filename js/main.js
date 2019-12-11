b0dd106fd9448f5fc830d0c605c0fefab27360e4
Tommaso Boggia <tboggia@gmail.com>
2019-12-11 00:11:24 -0800

    V1 ready

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-$(document).ready(function(){
+var emotion = document.forms.sentence.emotion,
+		behavior = document.forms.sentence.behavior;
 
+emotion.addEventListener('change', () => {
+	var emotionalState = emotion.selectedOptions[0].parentElement.label;
+	switch (emotionalState) {
+		case "Sadness & Grief":
+			document.body.id = "sadness"; break;
+		case "Anger & Frustration":
+			document.body.id = "anger"; break;
+		case "Fear & Anxiety":
+			document.body.id = "fear"; break;
+		case "Joy & Contentment":
+			document.body.id = "joy"; break;
+		default: 
+			document.body.id= "";
+	}
 });
 
-$('select').on('change',function(){
-	if ( $(this).attr('value') == "" ) {
-		$('body').css('color', 'purple');
-	} else {
-		$('body').css('color', 'red');
-	}
 
-})
+//  https://codepen.io/sunnysingh/pen/OPxbgq?editors=1010
+(function(){
+  var shareButtons = document.querySelectorAll(".share-btn");
+
+  if (shareButtons) {
+      [].forEach.call(shareButtons, function(button) {
+      button.addEventListener("click", function(event) {
+ 				var width = 650,
+            height = 450;
+
+        event.preventDefault();
+        var sentence = "&quote=When you " + behavior + ", it makes me feel " + emotion + ".";
+
+        document.querySelectorAll(".share-btn").forEach((a) => a.href += a.href + sentence);
+        window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width='+width+',height='+height+',top='+(screen.height/2-height/2)+',left='+(screen.width/2-width/2));
+      });
+    });
+  }
+
+})();
