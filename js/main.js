6d178f466b42455a7239d24f9f2f15a8264a936a
Tommaso Boggia <tboggia@gmail.com>
2019-12-10 22:15:24 -0800

    Committing what’s here

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
 
+});
+
+$('select').on('change',function(){
+	if ( $(this).attr('value') == "" ) {
+		$('body').css('color', 'purple');
+	} else {
+		$('body').css('color', 'red');
+	}
+
+})
