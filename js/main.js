2ad242295872f5b41cb164a8412bafdc05d65f70
Tommaso <tboggia@users.noreply.github.com>
2023-04-26 12:34:01 -0700

    Fix scriptlet

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+const iframe = document.getElementById('course-frame');
+const iframedoc = iframe.contentDocument? iframe.contentDocument: iframe.contentWindow.document;
+const inceptionframe = iframedoc.getElementById('scormdriver_content');
+const inceptiondoc = inceptionframe.contentDocument? inceptionframe.contentDocument: inceptionframe.contentWindow.document;
+const intervalID = setInterval(clickNext, 1000);
+const nextButton = inceptiondoc.getElementById('forwardButton');
+
+function is_next_clickable() {
+    console.log(nextButton);
+    return !nextButton.classList.contains('disabled');
+}
+
+function clickNext() {
+   if (is_next_clickable()) {
+       nextButton.click();
+   }
+}
+
+intervalID;
+
