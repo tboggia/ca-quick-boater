d78721383e036132af67d61009a633fbd186a670
Tommaso <tboggia@users.noreply.github.com>
2023-04-26 15:03:50 -0700

    Remove pauser, fixed Quix next press on quizzes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Use https://mrcoles.com/bookmarklet/ to turn it into a bookmarklet
 
 const iframe = document.getElementById('course-frame');
 const iframedoc = iframe.contentDocument? iframe.contentDocument: iframe.contentWindow.document;
@@ -5,16 +6,16 @@ const inceptionframe = iframedoc.getElementById('scormdriver_content');
 const inceptiondoc = inceptionframe.contentDocument? inceptionframe.contentDocument: inceptionframe.contentWindow.document;
 const intervalID = setInterval(clickNext, 1000);
 const nextButton = inceptiondoc.getElementById('forwardButton');
+const inceptionBody = inceptiondoc.body;
 
 function is_next_clickable() {
-    return !nextButton.classList.contains('disabled');
+    return !nextButton.classList.contains('disabled') && !inceptionBody.classList.contains('modal-open');
 }
 
 function clickNext() {
-   if (is_next_clickable()) {
-       nextButton.click();
-   }
+	if (is_next_clickable()) {
+			nextButton.click();
+	}
 }
 
 intervalID;
-
